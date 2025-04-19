fd1022e3757148306120175523221817cca21ec3
arazig <amine.razig@ensae.fr>
2025-04-19 14:17:54 +0000

    ajout yml et train fichier

---
+++ src/train.py
@@ -0,0 +1,44 @@
+# src/train.py
+
+import argparse
+import mlflow
+import mlflow.sklearn
+from src.data.load_data import S3DataLoader
+from src.models.train_model import ModelTuner
+
+BUCKET = "arazig"
+TRAIN_FILE = "projet-mise-en-prod/data/train.csv"
+TARGET_COL = "relative_humidity"
+
+
+def main(remote_server_uri: str, experiment_name: str):
+    mlflow.set_tracking_uri(remote_server_uri)
+    mlflow.set_experiment(experiment_name)
+
+    with mlflow.start_run():
+        # Log des paramètres
+        mlflow.log_param("bucket", BUCKET)
+        mlflow.log_param("train_file", TRAIN_FILE)
+
+        # Chargement
+        loader = S3DataLoader(BUCKET)
+        train_df = loader.load_csv(TRAIN_FILE)
+
+        # Entraînement avec tuning
+        tuner = ModelTuner(TARGET_COL)
+        model = tuner.fine_tune(train_df)
+
+        # Log du modèle et hyperparams
+        mlflow.log_params(tuner.best_params_)
+        mlflow.sklearn.log_model(model, "model")
+
+        print("✅ Modèle loggé avec succès !")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--remote_server_uri", type=str, required=True)
+    parser.add_argument("--experiment_name", type=str, required=True)
+    args = parser.parse_args()
+
+    main(args.remote_server_uri, args.experiment_name)
